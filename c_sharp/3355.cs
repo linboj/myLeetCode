241a0a85b8d78c4fdc1c9cbbd9ffb22ebd8d9bbb
linboj <spps2215@gmail.com>
2025-05-20 19:45:35 +0800

    3024, 3355

--- c_sharp/3355.cs
+++ c_sharp/3355.cs
@@ -23,3 +23,26 @@ public class Solution
         return true;
     }
 }
+
+public class Solution
+{
+    public bool IsZeroArray(int[] nums, int[][] queries)
+    {
+        int n = nums.Length, sum = 0;
+        int[] diffArray = new int[n + 1];
+
+        foreach (var query in queries)
+        {
+            diffArray[query[0]]++;
+            diffArray[query[1] + 1]--;
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += diffArray[i];
+            if (sum < nums[i])
+                return false;
+        }
+        return true;
+    }
+}
