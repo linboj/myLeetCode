db818fb13dbd86d9aaf3c1ec33ddbb9592fd9d47
linboj <spps2215@gmail.com>
2025-09-03 21:33:42 +0800

    3025, 3027

---
+++ c_sharp/3027.cs
@@ -0,0 +1,25 @@
+public class Solution
+{
+    public int NumberOfPairs(int[][] points)
+    {
+        Array.Sort(points, (a, b) => a[0] == b[0] ? b[1].CompareTo(a[1]) : a[0].CompareTo(b[0]));
+        int n = points.Length;
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int top = points[i][1];
+            int bottom = int.MinValue;
+            for (int j = i + 1; j < n; j++)
+            {
+                int y = points[j][1];
+                if (y > bottom && y <= top)
+                {
+                    ans++;
+                    bottom = y;
+                    if (top == bottom) break;
+                }
+            }
+        }
+        return ans;
+    }
+}
