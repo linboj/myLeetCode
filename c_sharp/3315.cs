fbc25f5ef9681701795bc338b8191c1002e1f629
linboj <spps2215@gmail.com>
2026-01-21 21:06:55 +0800

    3314, 3315

---
+++ c_sharp/3315.cs
@@ -0,0 +1,23 @@
+public class Solution
+{
+    public int[] MinBitwiseArray(IList<int> nums)
+    {
+        int n = nums.Count;
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            int num = nums[i];
+            int r = -1;
+            int d = 1;
+
+            while ((num & d) != 0)
+            {
+                r = num - d;
+                d <<= 1;
+            }
+            ans[i] = r;
+        }
+        return ans;
+    }
+}
